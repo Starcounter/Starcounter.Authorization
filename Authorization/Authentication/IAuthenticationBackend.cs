8b22d2b29bbcfd72004c09e608dfe917fe44c916
Marcin Goliński <mj.golinski@gmail.com>
2017-07-18 14:23:03 +0200

    Remove junk comments

--- Authorization/Authentication/IAuthenticationBackend.cs
+++ Authorization/Authentication/IAuthenticationBackend.cs
@@ -3,28 +3,6 @@ using Starcounter.Authorization.Core;
 
 namespace Starcounter.Authorization.Authentication
 {
-    //    // AuthorizationEnforcement?
-    //    public static partial class AuthorizationRules
-    //    {
-    //        public static bool Check<TPermission, T>(T obj) where TPermission : Permission<T>
-    //        {
-    //            List<Claim> claims = new List<Claim>();
-    //            var somebody = Authentication.GetCurrentPerson();
-    //            if (somebody != null)
-    //            {
-    //                claims.Add(new PersonClaim(somebody));
-    //            }
-    //
-    //            return GetPredicatesForPermission<TPermission>()
-    //                .Cast<Func<IEnumerable<Claim>, T, bool>>()
-    //                .Any(p => p(claims, obj));
-    //        }
-    //
-    //        public static bool Check<TPermission>() where TPermission : Permission<Unit>
-    //        {
-    //            return Check<TPermission, Unit>(null);
-    //        }
-    //    }
     public interface IAuthenticationBackend
     {
         IEnumerable<Claim> GetCurrentClaims();
