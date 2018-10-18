0f2dae9e4289a66645a36169dee85a7b973b8ca1
Marcin Goliński <mj.golinski@gmail.com>
2018-10-18 17:09:00 +0200

    Default AuthTicket TTL is now 15 minutes

--- Authorization/SignIn/SignInOptions.cs
+++ Authorization/SignIn/SignInOptions.cs
@@ -4,6 +4,6 @@ namespace Starcounter.Authorization.SignIn
 {
     internal class SignInOptions
     {
-        public TimeSpan NewTicketExpiration { get; set; } = TimeSpan.FromDays(7);
+        public TimeSpan NewTicketExpiration { get; set; } = TimeSpan.FromMinutes(15);
     }
 }
