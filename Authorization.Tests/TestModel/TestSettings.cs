3e2efcabc8c31462651ccbba31057065310c6132
Marcin Goliński <mj.golinski@gmail.com>
2018-10-23 14:47:41 +0200

    fix tests

---
+++ Authorization.Tests/TestModel/TestSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Starcounter.Authorization.Tests.TestModel
+{
+    public class TestSettings:IAuthorizationSettings
+    {
+        public long NewTicketExpirationSeconds { get; set; }
+        public long TicketCleanupIntervalSeconds { get; set; }
+    }
+}
