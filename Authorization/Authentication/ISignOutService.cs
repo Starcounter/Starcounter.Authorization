d3520253f58828e742b4e088a6dacd4f804810cc
Marcin Goliński <mj.golinski@gmail.com>
2018-10-30 16:53:44 +0100

    ISignOutService is now public

--- Authorization/Authentication/ISignOutService.cs
+++ Authorization/Authentication/ISignOutService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Starcounter.Authorization.Authentication
 {
-    internal interface ISignOutService
+    public interface ISignOutService
     {
         /// <summary>
         /// Signs the user out by removing the current authentication ticket. Does nothing if the user wasn't signed in.
