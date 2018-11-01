5b92b0276cf8ce5d7a6b1d098b0ba95efb0f66ab
Konstantin <const.miyang@gmail.com>
2018-11-01 14:11:03 +0100

    Format code in IScUserAuthenticationTicket.cs

--- Authorization/PublicInterfaces/Model/IScUserAuthenticationTicket.cs
+++ Authorization/PublicInterfaces/Model/IScUserAuthenticationTicket.cs
@@ -5,7 +5,7 @@ namespace Starcounter.Authorization
     /// <summary>
     /// Implement this interface with a class specific to your application to enable retrieving signed in user
     /// </summary>
-    public interface IScUserAuthenticationTicket<TUser>: IScAuthenticationTicket where TUser:IMinimalUser
+    public interface IScUserAuthenticationTicket<TUser> : IScAuthenticationTicket where TUser : IMinimalUser
     {
         TUser User { get; set; }
     }
