2f5bcb177df661c60e33e6f96da0d7ad87193b9d
Egor <egor.krushinov@starcounter.com>
2019-02-26 23:52:17 +0300

    Problem 89: nuspec file was added into project, AssemblyInfo was updated.

--- Authorization/AssemblyInfo.cs
+++ Authorization/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Starcounter.Authorization.Tests")]
+[assembly: InternalsVisibleTo("Starcounter.Authorization.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+[assembly: AssemblyTitle("Starcounter.Authorization")]
+[assembly: AssemblyDescription("Authorization helper library for Starcounter applications")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Starcounter AB")]
+[assembly: AssemblyProduct("Starcounter.Authorization")]
+[assembly: AssemblyCopyright("Copyright © 2019 Starcounter AB")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("3.7.5")]
+[assembly: AssemblyFileVersion("3.7.5")]
