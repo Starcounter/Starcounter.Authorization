cc1a9edda8423ca022b8b9947c3a9676410b38db
Martin Törnwall <martin@tornwall.net>
2019-06-10 10:55:13 +0200

    Restore AssemblyInfo to expose internals for test

---
+++ Authorization/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+// NOTE:
+// Assembly attributes are configured in the project file.
+// This file exists solely to expose internals to the test projects.
+
+[assembly: InternalsVisibleTo("Starcounter.Authorization.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
